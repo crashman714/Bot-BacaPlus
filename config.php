94a858c32a33e32063e07c6c4fb500faa1764f1f
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-01-26 09:30:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_1243181399172488";
+$raw_uid="76a21775-5e2c-4715-b23d-bc750f2a0715";
+$user_name="Febry+Afriansyah";
+$invite_code="F5h0u9SLOq";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="6912c3a6c7981e41";
+$device_type="Xiaomi++Redmi+4A";
+
+
+?>
