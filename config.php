c2a39107935623ba180bae68b1405a31ccd4fe8c
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-02-03 07:02:47 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-
+// ganti semua datanya dengan punya kalian //
+// Untuk Yang Belum Tau Cara Lihat Data Ini PM gw //
+// 082386372115 //
 
 $uid="Facebook_1243181399172488";
 $raw_uid="76a21775-5e2c-4715-b23d-bc750f2a0715";
@@ -8,6 +10,5 @@ $invite_code="F5h0u9SLOq";
 $own_mac="02%3A00%3A00%3A00%3A00%3A00";
 $android_id="6912c3a6c7981e41";
 $device_type="Xiaomi++Redmi+4A";
-
 
 ?>
